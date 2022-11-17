17bfe60d48c0e2a26deda860e97d8402c959f636
boo <borcunozkablan@gmail.com>
2022-11-17 18:40:27 +0300

    readme is being written

--- test/message-test.cpp
+++ test/message-test.cpp
@@ -1,8 +1,8 @@
 #include "messageFactory.h"
 #include <gtest/gtest.h>
 
-#define MESSAGE_TOPIC_1  ("my-test-topic-1")
-#define MESSAGE_TOPIC_2  ("my-test-topic-2")
+#define MESSAGE_TOPIC_1   ("my-test-topic-1")
+#define MESSAGE_TOPIC_2   ("my-test-topic-2")
 #define MESSAGE_PAYLOAD_1 ("This is my first payload")
 #define MESSAGE_PAYLOAD_2 ("This is my second payload")
 
