4db2dd8c811ed9b9eacf53294960c0cb4f01444a
boo <borcunozkablan@gmail.com>
2022-11-20 23:31:39 +0300

    comment is added

--- example/main.cpp
+++ example/main.cpp
@@ -3,14 +3,21 @@
 #include "rssReader.h"
 #include <thread>
 
+//! subscriber count
 #define READER_COUNT  (4)
+
+//! publisher count
 #define FEEDER_COUNT  (4)
 
+// feeder simulation function
 void feed(RSSFeeder feeder, Message message);
 
+
 int main(int argc, char **argv) {
+  // create a topic by using factory class
   Topic topic = TopicFactory::getInstance()->create();
-  
+
+  // create subscribers
   RSSReader readers[READER_COUNT] = {
     RSSReader("Reader-1"),
     RSSReader("Reader-2"),
@@ -18,23 +25,28 @@ int main(int argc, char **argv) {
     RSSReader("Reader-4")
   };
   
+  // create publishers
   RSSFeeder feeders[FEEDER_COUNT] = {
     RSSFeeder("Feeder-1"),
     RSSFeeder("Feeder-2"),
     RSSFeeder("Feeder-3"),
     RSSFeeder("Feeder-4")
   };
-
+  
+  // subscribe all subscribers to one topic
   for (int i = 0; i < READER_COUNT; ++i) {
     topic.subscribe(&readers[i]);
   }
 
+  std::cout << "Topic" << std::endl << "-----" << std::endl;
+  std::cout << topic << std::endl;
+
+  // set one topic to all publishers
   for (int i = 0; i < FEEDER_COUNT; ++i) {
     feeders[i].setTopic(&topic);
   }
 
-  std::cout << topic << std::endl;
-
+  // create threads for feeders with different messages, then run them
   std::thread threads[FEEDER_COUNT] = {
     std::thread(feed, feeders[0], Message("Message from Feeder-1")),
     std::thread(feed, feeders[1], Message("Message from Feeder-2")),
@@ -49,8 +61,9 @@ int main(int argc, char **argv) {
   return 0;
 }
 		
-void feed(RSSFeeder feeder, Message message) {
-  for (int i = 0; i < 3; ++i) {
+void feed(RSSFeeder feeder, Message message) {  
+  /// @attention you might increase arbitrary limit value to view concurrency better
+  for (int i = 0; i < 4; ++i) {
     feeder.notify(message);
   }
   
